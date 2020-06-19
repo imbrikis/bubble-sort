0b6b40d43fb08ec8fd1732a5f87814245cb2c88c
imbrikis <imbrikis@yahoo.com>
2020-06-18 21:15:50 -0600

    initial commit

---
+++ BubbleSort.js
@@ -0,0 +1,29 @@
+// ---- TEST ARRAY ---- //
+var arr = [90, 6, 723, 1, 99, 4087, 33, 297, 8, 45, 963]
+
+var isUnsorted = true
+
+function parseArray(arr) {
+	isUnsorted = false
+  for (var x = 0; x < arr.length; x++) {
+    if (arr[x] > arr[x + 1]) {
+      isUnsorted = true
+      var y = arr[x]
+      var z = arr[x + 1]
+      arr[x + 1] = y
+      arr[x] = z
+    } 
+  }
+  bubbleSort(arr)
+}
+
+function bubbleSort(arr) {
+  if (isUnsorted) {
+  	parseArray(arr)
+  } else {
+  	return arr
+  }
+}
+
+console.log(bubbleSort(arr))
+console.log(arr)
