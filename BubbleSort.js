3a0a10c90d26afc4cec95b7366fa94edf5464cd5
imbrikis <imbrikis@yahoo.com>
2021-01-12 22:50:50 -0700

    refactor and simplify logic

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,15 +1,15 @@
 // ---- TEST ARRAY ---- //
-var arr = [90, 6, 723, 1, 99, 4087, 33, 297, 8, 45, 963]
+let arr = [90, 6, 723, 1, 99, 4087, 33, 297, 8, 45, 963]
 
-var isUnsorted = true
+let arrIsUnsorted = true
 
-function parseArray(arr) {
-	isUnsorted = false
-  for (var x = 0; x < arr.length; x++) {
+const parseArray = arr => {
+	arrIsUnsorted = false
+  for (let x = 0; x < arr.length; x++) {
     if (arr[x] > arr[x + 1]) {
-      isUnsorted = true
-      var y = arr[x]
-      var z = arr[x + 1]
+      arrIsUnsorted = true
+      let y = arr[x]
+      let z = arr[x + 1]
       arr[x + 1] = y
       arr[x] = z
     } 
@@ -17,13 +17,11 @@ function parseArray(arr) {
   bubbleSort(arr)
 }
 
-function bubbleSort(arr) {
-  if (isUnsorted) {
-  	parseArray(arr)
-  } else {
-  	return arr
-  }
-}
+const bubbleSort = arr => arrIsUnsorted ? parseArray(arr) : arr
+
+console.log(`The unsorted array: ${arr}
+Now running the bubble sort algorithm against the array...`)
 
 bubbleSort(arr)
-console.log(arr)
+
+console.log(`The array has been sorted: ${arr}`)
